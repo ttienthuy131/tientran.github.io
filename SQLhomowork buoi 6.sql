60a2302de285fb461a9e70fc821f20af48252f7c
ttienthuy131 <thuytien01121998@gmail.com>
2024-08-15 16:13:55 +0700

    Update SQLhomowork buoi 6.sql

--- SQLhomowork buoi 6.sql
+++ SQLhomowork buoi 6.sql
@@ -1,16 +1,1 @@
-SELECT TOP 10 PERCENT
-   tra.transaction_id,
-   SUM(tra.charged_amount) AS total_amount
-FROM
-   dbo.fact_transaction_2019 AS tra
-LEFT JOIN
-   dbo.dim_scenario AS sce ON tra.scenario_id = sce.scenario_id
-LEFT JOIN
-   dbo.dim_status AS sta ON sta.status_id = tra.status_id
-WHERE
-   MONTH(tra.transaction_time) = 2 AND
-   sta.status_description <> 'success'
-GROUP BY
-   tra.transaction_id
-ORDER BY
-   total_amount DESC;
+
